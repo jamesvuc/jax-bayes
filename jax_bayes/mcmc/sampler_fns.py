0c533be71d1dc0d10d8414c346a8d220405539a3
James Vuckovic <james@jamesvuckovic.com>
2022-11-23 12:05:32 -0500

    Fix a minor error with the log prob for the MALA when noise scale is not 1. Add rms_mala to the sampler_fns.

--- jax_bayes/mcmc/sampler_fns.py
+++ jax_bayes/mcmc/sampler_fns.py
@@ -117,7 +117,7 @@ def mala_fns(
         x, = x
         xprop, = xprop
         return - 0.5 * jnp.sum(jnp.square((xprop - x - step_size(i) * g)) \
-                / math.sqrt(2 *step_size(i) * noise_scale(i)))
+                / 2 * step_size(i) * noise_scale(i)**2)
     log_proposal = jax.vmap(log_proposal, in_axes=(None, 0, 0, 0, 0))
 
     def init(x0, key):
@@ -371,6 +371,87 @@ def rms_langevin_fns(
     return init, propose, log_proposal, update, get_params, base_key
 
 @sampler
+def rms_mala_fns(
+    base_key,
+    num_samples=10,
+    step_size=1e-3,
+    noise_scale=1.0,
+    beta=0.9,
+    eps=1e-9,
+    init_stddev=0.1,
+    init_dist='normal'
+):
+    """Constructs sampler functions for the Metropolis Adjusted Langevin Algorithm.
+    See e.g. http://probability.ca/jeff/ftpdir/lang.pdf
+
+    Args:
+        base_key: jax.random.PRNGKey
+        num_samples: number of samples to initialize; either > 0 or -1.
+            If num_samples == -1, assumes that the initial samples are
+            already constructed.
+        step_size: float or callable w/ signature step_size(i)
+        init_stdev: nonnegative float standard deviation for initialization
+            of initial samples (ignored if num_samples == -1)
+        init_dist: str in ['normal', 'centered_uniform'] to sample perturbations
+            for the initial distribution
+
+    Returns:
+        sampler function tuple (init, propose, update, get_params)
+    """
+    step_size = make_schedule(step_size)
+    noise_scale = make_schedule(noise_scale)
+
+    if isinstance(init_dist, str):
+        init_dist = init_distributions[init_dist]
+
+    def log_proposal(i, g, x, gprop, xprop): #grads come first
+        #computes log q(xprop|x)
+        x,r = x
+        xprop,rprop = xprop
+        ss = step_size(i) / (jnp.sqrt(r) + eps)
+        return - 0.5 * jnp.sum(jnp.square(xprop - x - ss * g) \
+                / 2 * ss * noise_scale(i)**2)
+    log_proposal = jax.vmap(log_proposal, in_axes=(None, 0, 0, 0, 0))
+
+    def init(x0, key):
+        init_key, next_key = jax.random.split(key)
+        if num_samples == -1:
+            r = jnp.zeros_like(x0)
+            return (x0, r), next_key
+        x = init_dist(init_key, (num_samples,) + x0.shape)
+        r = jnp.zeros_like(x)
+        return (x0 + init_stddev * x, r), next_key
+
+    def propose(i, g, x, key, **kwargs):
+        key, next_key = jax.random.split(key)
+        x, r = x
+        Z = jax.random.normal(key, x.shape)
+
+        r = beta * r + (1. - beta) * jnp.square(g)
+
+        ss = step_size(i) / (jnp.sqrt(r) + eps)
+        xprop = x + ss * g + jnp.sqrt(2 * ss) * noise_scale(i) * Z
+
+        return (xprop, r), next_key
+
+    def update(i, accept_idxs, g, x, gprop, xprop, key):
+        """ if the state had additional data, you would need to accept them too"""
+        x, r = x
+        xprop, rprop = xprop
+
+        accept_idxs = match_dims(accept_idxs, x)
+        mask = accept_idxs.astype(jnp.float32)
+
+        xnext = x * (1.0 - mask) + xprop * mask
+        rnext = r * (1.0 - mask) + rprop * mask
+        return (xnext, rnext), key
+
+    def get_params(x):
+        return x[0]
+
+    return init, propose, log_proposal, update, get_params, base_key
+
+@sampler
 def rwmh_fns(
     base_key,
     num_samples=10,
@@ -402,7 +483,6 @@ def rwmh_fns(
     def log_proposal(*args):
         # in this case, the proposal is symmetric, so this is correct
         return jnp.zeros(num_samples)
-    # log_proposal = jax.vmap(log_proposal, in_axes=(None, 0, 0, 0, 0))
 
     def init(x0, key):
         init_key, next_key = jax.random.split(key)
