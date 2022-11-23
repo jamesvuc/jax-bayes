d5c403b8792be6dbb4ffe0e579b59295ec545d95
James Vuckovic <james@jamesvuckovic.com>
2022-11-23 12:09:07 -0500

    Add rms_langevin_fns import.

--- jax_bayes/mcmc/__init__.py
+++ jax_bayes/mcmc/__init__.py
@@ -3,6 +3,7 @@ from .sampler_fns import mala_fns
 from .sampler_fns import rk_langevin_fns
 from .sampler_fns import hmc_fns
 from .sampler_fns import rms_langevin_fns
+from .sampler_fns import rms_mala_fns
 from .sampler_fns import rwmh_fns
 
 from .utils import blackbox_mcmc, init_distributions
